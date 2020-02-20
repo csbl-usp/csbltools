a71edddf9e1bb3e79472d8db74b8df2b730fcc96
André Nicolau <anicolau85@gmail.com>
2020-02-20 09:43:17 -0300

    Adding possibility to search by OR or AND
    Adding a parameter logical_operator to define logical operator OR or AND to search.

--- R/search_multiple_names.R
+++ R/search_multiple_names.R
@@ -3,8 +3,8 @@
 #'  Wrapper to search match in a R vector for many words
 #'
 #' @param vector_of_interest The vector that you want to perform a search in.
-#' @param query_vector A vector of the words that you want to match (at least one of them)
-#' @param perl Logical (TRUE or FALSE). Indicates whether perl-compatible regex will be used.
+#' @param query_vector A vector of the words that you want to match (at least one of them).
+#' @param logical_operator A string of logical operator to define search criteria to query_vector.
 #' Defaults to \code{FALSE}.
 #' @param return_index Logical (TRUE or FALSE). Indicates whether the search will return indexes or the values themselves in the vector.
 #' Defaults to false (thus values are returned).
@@ -18,8 +18,13 @@
 #' search_multiple_names(vector_of_interest, query_vector = query, return_index = TRUE)
 
 
-search_multiple_names <- function(vector_of_interest, query_vector, ...) {
+search_multiple_names <- function(vector_of_interest, query_vector, logical_operator = c("OR", "AND"), return_index = FALSE, ...) {
+  if(logical_operator == "OR") {
+    query_string <- paste(query_vector, collapse = "|")
+  } else if(logical_operator == "AND") {
+    query_string <- paste(query_vector, collapse = "&")
+  }
   return(
-    grep(pattern = paste(query_vector, collapse = "|"), x = vector_of_interest, perl = perl, value = !return_index)
+    grep(pattern = query_string, x = vector_of_interest, perl = TRUE, value = !return_index)
   )
 }
