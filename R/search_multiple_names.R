7dc2ad2b61ab3df3684559b4666025ef37ef97e9
Andre Nicolau <anicolau85@gmail.com>
2020-02-21 16:52:59 -0300

    logical  operator was removed and fixed examples

--- R/search_multiple_names.R
+++ R/search_multiple_names.R
@@ -4,26 +4,21 @@
 #'
 #' @param vector_of_interest The vector that you want to perform a search in.
 #' @param query_vector A vector of the words that you want to match (at least one of them).
-#' @param logical_operator A string of logical operator to define search criteria to query_vector.
 #' Defaults to \code{FALSE}.
 #' @param return_index Logical (TRUE or FALSE). Indicates whether the search will return indexes or the values themselves in the vector.
 #' Defaults to false (thus values are returned).
 #' @examples
-#' # Vector de interesse para realizar a busca
+#' # Vector contains data to be filtered my search vector
 #' csblers <- c("Andre Nicolau", "Andre Guilherme", "Deney Muleque Doido", "Thiago Hirata", "Tiago Lubiana")
-#' # Vector contendo os valores que serão buscados
+#' # Vector contains term to find in vector of interest
 #' query <- c("Thiago", "Tiago")
-#' # Comando grep para buscar os valores de interesse
-#' search_multiple_names(vector_of_interest, query_vector = query)
-#' search_multiple_names(vector_of_interest, query_vector = query, return_index = TRUE)
+#' # Function command to find one or more values in other vector
+#' search_multiple_names(vector_of_interest = csblers, query_vector = query)
+#' search_multiple_names(vector_of_interest = csblers, query_vector = query, return_index = TRUE)
 
 
-search_multiple_names <- function(vector_of_interest, query_vector, logical_operator = c("OR", "AND"), return_index = FALSE, ...) {
-  if(logical_operator == "OR") {
-    query_string <- paste(query_vector, collapse = "|")
-  } else if(logical_operator == "AND") {
-    query_string <- paste(query_vector, collapse = "&")
-  }
+search_multiple_names <- function(vector_of_interest, query_vector, return_index = FALSE, ...) {
+  query_string <- paste(query_vector, collapse = "|")
   return(
     grep(pattern = query_string, x = vector_of_interest, perl = TRUE, value = !return_index)
   )
